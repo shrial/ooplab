0ffb9e521e56635022e0fdee1659907572813adf
Shrika <147275125+shrial@users.noreply.github.com>
2024-09-12 03:48:38 +0530

    Update Searcharr.java

--- lab4/Searcharr.java
+++ lab4/Searcharr.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 public class Searcharr {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.println("enter the number of rows");     
+        System.out.print("enter number of rows");     
         int m=in.nextInt();    
-        System.out.println("enter the number of columns");     
-        int n=in.nextInt(); 
-        System.out.println("enter the value to be found");
-        int key=in.nextInt();
+        System.out.print("enter number of columns");     
+        int n=in.nextInt();        
         int arr[][]=new int[m][n],count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                System.out.println("enter the element");
+                System.out.print("enter element");
                 arr[i][j]=in.nextInt();
+            }
+        }
+        System.out.println("enter value to be found");
+        int key=in.nextInt();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(arr[i][j]==key){
                     count++;
                 }
-            
             }
         }
-        System.out.println("number of occurence of "+key+" is "+count);
+        System.out.println("number of occurrence of "+key+" is "+count);
         in.close();
     }
 }
